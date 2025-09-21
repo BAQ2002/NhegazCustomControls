26cb1c102b1e0b2b8a5eb8aa7dac10b126f63cf0
BAQ2002 <antoniobaqueirocosta@gmail.com>
2025-09-21 10:47:16 -0300

    update

--- NhegazCustomControls/CustomControlBase/CustomControl.cs
+++ NhegazCustomControls/CustomControlBase/CustomControl.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Drawing.Drawing2D;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
 
 namespace NhegazCustomControls
 {
